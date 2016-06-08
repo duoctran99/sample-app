264fb666aecec88fcc6762700228d44968c13df6
Luc <luc.dangquang@rainmaker-labs.com>
2016-06-08 23:30:30 +0700

    Finish static pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
 end
+
+
+# ------------- When checkout new branch should -------------
+# git push -u origin static-pages
+# git push
+# 1 - create new "static-pages" branch on server && usually to push
